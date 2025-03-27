5d894178e79696ab47863db6cf877aa4c4f4f31e
Matthew Dlugosz <dlugoszmatthew@gmail.com>
2025-03-27 10:46:45 -0500

    ready for merge

--- app/page.tsx
+++ app/page.tsx
@@ -2,24 +2,13 @@
 
 import { TimerDisplay } from '@/components/timer-display'
 import { DashboardStats } from '@/components/dashboard-stats'
-import { ActivityGrid } from '@/components/activity-grid'
-import { useAuth } from '@/contexts/auth-context'
 
 export default function Home() {
-  const { user } = useAuth()
-
   return (
     <div className="container max-w-5xl mx-auto px-4 py-8">
       <DashboardStats />
 
       <TimerDisplay />
-      
-      {/* Only show activity grid for logged in users */}
-      {user && (
-        <ActivityGrid />
-      )}
-      
-      
     </div>
   )
 }
